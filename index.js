3a6f349216f614977f6ed716e06568114b1e30a9
Jazlin Yu <jazlinsub@gmail.com>
2023-08-01 09:36:30 -0700

    passing test

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hqBlock = 42;
+
+function distanceFromHqInBlocks(startBlock) {
+  const numberOfBlocks = parseInt(startBlock);
+  return Math.abs(numberOfBlocks - hqBlock);
+}
+
+function distanceFromHqInFeet(startBlock) {
+  let numberOfFeet = 0;
+  numberOfFeet = distanceFromHqInBlocks(startBlock) * 264;
+  return numberOfFeet;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  let blocksTraveledInFeet = Math.abs(startBlock - endBlock) * 264;
+  return blocksTraveledInFeet;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  let fareDistance = distanceTravelledInFeet(startBlock, endBlock);
+  let farePrice;
+
+  if (fareDistance <= 400) {
+    return 0;
+  } else if (fareDistance > 400 && fareDistance <= 2000) {
+    farePrice = (fareDistance - 400) * 0.02;
+  } else if (fareDistance > 2000 && fareDistance <= 2500) {
+    farePrice = 25;
+  } else {
+    farePrice = "cannot travel that far";
+  }
+
+  return farePrice;
+}
